dd6aafac4e0939cb1407ee47229f504b4e38cddd
Ahmed Samir <ahmed.samir@espace.com.eg>
2022-10-05 00:01:57 +0200

    Initial Commit

---
+++ Breaking Bad/Breaking_BadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Breaking_BadApp.swift
+//  Breaking Bad
+//
+//  Created by Samir on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Breaking_BadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
